3ddc2f5104bacde6a07c95ddab6504a1170fc713
Han <ajr7875@naver.com>
2019-10-09 16:53:02 +0900

    Drill 5 완성
    거리가멀면 멀수록 속도가 빨라서 일정한 속도로 움직이게 함.
    2014182045 한동주 최종 커밋 횟수 6회
    https://youtu.be/ONO5Lg0cdMg

--- Drills/Drill-5/move_character_with_mouse.py
+++ Drills/Drill-5/move_character_with_mouse.py
@@ -1,4 +1,5 @@
 from pico2d import *
+import math
 
 KPU_WIDTH, KPU_HEIGHT = 1280, 1024
 
@@ -6,17 +7,21 @@ KPU_WIDTH, KPU_HEIGHT = 1280, 1024
 def move(move_x,move_y):
     global x,y
     global i
-    global  animation_line
-    if i<= 100:
-        t = i / 100
-        x = (1 - t) * x + t * move_x
-        y = (1 - t) * y + t * move_y
-        i+=1
-    else:
-        if animation_line == 1:
-            animation_line = 3
-        elif animation_line == 0:
-            animation_line =2
+    global animation_line
+
+    global dist
+    if dist != 0:
+        if i<= dist:
+            t = i / dist
+            x = (1 - t) * x + t * move_x
+            y = (1 - t) * y + t * move_y
+            i+=1
+
+        else:
+            if animation_line == 1:
+                animation_line = 3
+            elif animation_line == 0:
+                animation_line =2
 
 def handle_events():
     global running
@@ -25,6 +30,7 @@ def handle_events():
     global mouse_x, mouse_y
     global i
     global animation_line
+    global dist
 
     events = get_events()
     for event in events:
@@ -34,6 +40,10 @@ def handle_events():
         elif event.type == SDL_MOUSEBUTTONDOWN:
             if event.button == SDL_BUTTON_LEFT:
                 move_x, move_y = event.x, KPU_HEIGHT - 1 - event.y
+
+                dist = math.sqrt((move_x-x)*(move_x-x) + (move_y-y)*(move_y-y))
+                dist = int(dist)
+
                 i = 0
                 if x<move_x:
                     animation_line = 1
@@ -58,6 +68,7 @@ mouse_x , mouse_y = 0, 0
 animation_line = 1
 x, y = KPU_WIDTH // 2, KPU_HEIGHT // 2
 move_x,move_y = x,y
+dist = 0
 i = 0
 frame = 0
 hide_cursor()
@@ -72,6 +83,7 @@ while running:
     frame = (frame + 1) % 8
 
     handle_events()
+    delay(0.01)
 
 close_canvas()
 
