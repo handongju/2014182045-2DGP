3b9d0f7ac53bdaf240b3c4916ffe1ba2d182b92f
Han <ajr7875@naver.com>
2019-10-09 16:35:47 +0900

    Update move_character_with_mouse.py
    마우스 클릭시 이동 추가

--- Drills/Drill-5/move_character_with_mouse.py
+++ Drills/Drill-5/move_character_with_mouse.py
@@ -3,16 +3,34 @@ from pico2d import *
 KPU_WIDTH, KPU_HEIGHT = 1280, 1024
 
 
+def move(move_x,move_y):
+    global x,y
+    global i
+    if i<= 100:
+        t = i / 100
+        x = (1 - t) * x + t * move_x
+        y = (1 - t) * y + t * move_y
+        i+=1
+        print(x,y,i,move_x,move_y)
+
 def handle_events():
     global running
-    global x, y
+    global move_x, move_y
     global mouse_x, mouse_y
+    global i
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
+
+        elif event.type == SDL_MOUSEBUTTONDOWN:
+            if event.button == SDL_BUTTON_LEFT:
+                move_x, move_y = event.x, KPU_HEIGHT - 1 - event.y
+                i = 0
+
         elif event.type == SDL_MOUSEMOTION:
             mouse_x, mouse_y = event.x, KPU_HEIGHT -1 - event.y
+
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
     pass
@@ -26,6 +44,8 @@ character = load_image('animation_sheet.png')
 running = True
 mouse_x , mouse_y = 0, 0
 x, y = KPU_WIDTH // 2, KPU_HEIGHT // 2
+move_x,move_y = x,y
+i = 0
 frame = 0
 hide_cursor()
 
@@ -34,7 +54,7 @@ while running:
     kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
     character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
     mouse_pointer.clip_draw(0,0,52,52,mouse_x,mouse_y)
-    mouse_pointer.clip_draw
+    move(move_x,move_y)
     update_canvas()
     frame = (frame + 1) % 8
 
