18244a91c05db54cdcc04602fb063bf28a4901d9
Han <ajr7875@naver.com>
2019-10-21 13:54:16 +0900

    대쉬 추가
    Dash state 추가

---
+++ Drills/Drill-9/boy.py
@@ -0,0 +1,227 @@
+from pico2d import *
+
+# Boy Event
+RIGHT_DOWN, LEFT_DOWN, RIGHT_UP, LEFT_UP, SLEEP_TIMER, RSHIFT_DOWN, RSHIFT_UP, LSHIFT_DOWN, LSHIFT_UP = range(9)
+
+key_event_table = {
+    (SDL_KEYDOWN, SDLK_RIGHT): RIGHT_DOWN,
+    (SDL_KEYDOWN, SDLK_LEFT): LEFT_DOWN,
+    (SDL_KEYUP, SDLK_RIGHT): RIGHT_UP,
+    (SDL_KEYUP, SDLK_LEFT): LEFT_UP,
+    (SDL_KEYDOWN, SDLK_RSHIFT): RSHIFT_DOWN,
+    (SDL_KEYUP, SDLK_RSHIFT): RSHIFT_UP,
+    (SDL_KEYDOWN, SDLK_LSHIFT): LSHIFT_DOWN,
+    (SDL_KEYUP, SDLK_LSHIFT): LSHIFT_UP
+}
+
+
+
+# Boy States
+class IdleState:
+    frame_speed = 0
+
+    @staticmethod
+    def enter(boy, event):
+        if event == RIGHT_DOWN:
+            boy.velocity += 1
+        elif event == LEFT_DOWN:
+            boy.velocity -= 1
+        elif event == RIGHT_UP:
+            boy.velocity -= 1
+        elif event == LEFT_UP:
+            boy.velocity += 1
+        boy.timer = 1000
+
+    @staticmethod
+    def exit(boy, event):
+        pass
+
+    @staticmethod
+    def do(boy):
+        IdleState.frame_speed += 1
+
+        if IdleState.frame_speed > 30:
+            boy.frame = (boy.frame + 1) % 8
+            IdleState.frame_speed = 0
+
+        boy.timer -= 1
+        if boy.timer == 0:
+            boy.add_event(SLEEP_TIMER)
+
+    @staticmethod
+    def draw(boy):
+        if boy.dir == 1:
+            boy.image.clip_draw(boy.frame * 100, 300, 100, 100, boy.x, boy.y)
+        else:
+            boy.image.clip_draw(boy.frame * 100, 200, 100, 100, boy.x, boy.y)
+
+
+class RunState:
+    frame_speed = 0
+
+    @staticmethod
+    def enter(boy, event):
+        if event == RIGHT_DOWN:
+            boy.velocity += 1
+        elif event == LEFT_DOWN:
+            boy.velocity -= 1
+        elif event == RIGHT_UP:
+            boy.velocity -= 1
+        elif event == LEFT_UP:
+            boy.velocity += 1
+        boy.dir = boy.velocity
+
+    @staticmethod
+    def exit(boy, event):
+        pass
+
+    @staticmethod
+    def do(boy):
+        RunState.frame_speed += 1
+
+        if RunState.frame_speed > 30:
+            boy.frame = (boy.frame + 1) % 8
+            RunState.frame_speed = 0
+        boy.timer -= 1
+        boy.x += boy.velocity
+        boy.x = clamp(25, boy.x, 800 - 25)
+
+    @staticmethod
+    def draw(boy):
+        if boy.velocity == 1:
+            boy.image.clip_draw(boy.frame * 100, 100, 100, 100, boy.x, boy.y)
+        else:
+            boy.image.clip_draw(boy.frame * 100, 0, 100, 100, boy.x, boy.y)
+
+class DashState:
+    frame_speed = 0
+
+    @staticmethod
+    def enter(boy, event):
+        boy.timer = 300
+
+    @staticmethod
+    def exit(boy, event):
+        pass
+
+    @staticmethod
+    def do(boy):
+        RunState.frame_speed += 1
+
+        if RunState.frame_speed > 10:
+            boy.frame = (boy.frame + 1) % 8
+            RunState.frame_speed = 0
+        boy.timer -= 1
+        boy.x += boy.velocity * 1.3
+        boy.x = clamp(25, boy.x, 800 - 25)
+        if boy.timer == 0:
+            boy.add_event(LSHIFT_UP)
+
+    @staticmethod
+    def draw(boy):
+        if boy.velocity == 1:
+            boy.image.clip_draw(boy.frame * 100, 100, 100, 100, boy.x, boy.y)
+        else:
+            boy.image.clip_draw(boy.frame * 100, 0, 100, 100, boy.x, boy.y)
+
+class SleepState:
+    frame_speed = 0
+
+    @staticmethod
+    def enter(boy, event):
+        boy.frame = 0
+
+    @staticmethod
+    def exit(boy, event):
+        pass
+    @staticmethod
+    def do(boy):
+        SleepState.frame_speed+=1
+        if SleepState.frame_speed > 100:
+            boy.frame = (boy.frame + 1) % 8
+            SleepState.frame_speed = 0
+    @staticmethod
+    def draw(boy):
+        if boy.dir == 1:
+            boy.image.clip_composite_draw(boy.frame * 100, 300, 100, 100,
+                                          3.141592 / 2, '', boy.x - 25, boy.y - 25, 100, 100)
+        else:
+            boy.image.clip_composite_draw(boy.frame * 100, 200, 100, 100,
+                                          -3.141592 / 2, '', boy.x + 25, boy.y - 25, 100, 100)
+
+
+next_state_table = {
+    IdleState: {RIGHT_UP: RunState, LEFT_UP: RunState,
+                RIGHT_DOWN: RunState, LEFT_DOWN: RunState,
+                SLEEP_TIMER: SleepState,
+                RSHIFT_DOWN: IdleState, LSHIFT_DOWN: IdleState,
+                RSHIFT_UP: IdleState, LSHIFT_UP: IdleState},
+
+    RunState: {RIGHT_UP: IdleState, LEFT_UP: IdleState,
+               LEFT_DOWN: IdleState, RIGHT_DOWN: IdleState,
+               RSHIFT_DOWN: DashState, LSHIFT_DOWN : DashState,
+               RSHIFT_UP: RunState, LSHIFT_UP: RunState},
+
+    DashState: {RIGHT_UP: IdleState, LEFT_UP: IdleState,
+                LEFT_DOWN: IdleState, RIGHT_DOWN: IdleState,
+                RSHIFT_DOWN: DashState, LSHIFT_DOWN : DashState,
+                RSHIFT_UP: RunState, LSHIFT_UP: RunState},
+
+    SleepState: {LEFT_DOWN: RunState, RIGHT_DOWN: RunState,
+                 LEFT_UP: RunState, RIGHT_UP: RunState,
+                 RSHIFT_DOWN: IdleState, LSHIFT_DOWN : IdleState,
+                 RSHIFT_UP: IdleState, LSHIFT_UP: IdleState}
+}
+
+
+
+
+
+
+
+class Boy:
+
+    def __init__(self):
+        self.x, self.y = 800 // 2, 90
+        self.image = load_image('animation_sheet.png')
+        self.dir = 1
+        self.velocity = 0
+        self.frame = 0
+        self.timer = 0
+        self.event_que = []
+        self.cur_state = IdleState
+        self.cur_state.enter(self, None)
+
+
+    def change_state(self,  state):
+        if len(self.event_que) > 0:
+            event = self.event_que.pop()
+            self.cur_state.exit(self, event)
+            self.cur_state = next_state_table[self.cur_state][event]
+            self.cur_state.enter(self, event)
+
+
+    def add_event(self, event):
+        self.event_que.insert(0, event)
+
+
+    def update(self):
+        self.cur_state.do(self)
+        if len(self.event_que) > 0:
+
+            event = self.event_que.pop()
+
+            self.cur_state.exit(self, event)
+            self.cur_state = next_state_table[self.cur_state][event]
+            self.cur_state.enter(self, event)
+
+
+    def draw(self):
+        self.cur_state.draw(self)
+
+
+    def handle_event(self, event):
+        if (event.type, event.key) in key_event_table:
+            key_event = key_event_table[(event.type, event.key)]
+            self.add_event(key_event)
+
